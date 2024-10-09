16e0cd546036d0cbe0e4509f5a223ec51d016581
cohamil <coohamil@ucsc.edu>
2024-10-09 00:09:18 -0700

    Step 3: added automatic clicking

--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,33 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// Counter variables
+let counter: number = 0;
+const counterDisplay = document.getElementById("counter-display");
+
 // Add incremental button
-let counter: number = 0; 
+const buttonClickAmount = 1;
 const button = document.createElement("button");
 button.textContent = "💎 Click for Gems! 💎";
 app.append(button);
 button.addEventListener("click", () => {
-    counter += 1;
+    counter += buttonClickAmount;
+    UpdateCounterDisplay(counterDisplay, counter);
+});
 
-    // Update the counter display
-    const counterDisplay = document.getElementById("counter-display");
+// Automatic clicking
+const autoClickDelayInSecs = 1;
+const autoClickAmount = 1;
+setInterval(() => {
+    counter += autoClickAmount;
+    UpdateCounterDisplay(counterDisplay, counter);
+}, autoClickDelayInSecs * 1000);
+
+// Update the counter display
+function UpdateCounterDisplay(counterDisplay: HTMLElement | null, counter: number) {
     if (counterDisplay) {
         counterDisplay.textContent = `Gems: ${counter}`;
     } else {
         console.error("Counter display not found!");
     }
-})
+}
