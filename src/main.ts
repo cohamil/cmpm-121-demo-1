f4263aa1e2c46c8f76ab56df4d61086f90f502ca
cohamil <coohamil@ucsc.edu>
2024-10-08 22:56:59 -0700

    Step 1: added a clickable button

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,9 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+
+// Add incremental button
+const button = document.createElement("button");
+button.textContent = "💵 Click for Money! 💵";
+app.append(button);
