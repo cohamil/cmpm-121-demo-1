e70db382c17e0db198dfb70aaf4bacf78a6ea543
cohamil <coohamil@ucsc.edu>
2024-10-08 23:48:23 -0700

    Step 2: added a counter for button

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,19 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-
 // Add incremental button
+let counter: number = 0; 
 const button = document.createElement("button");
-button.textContent = "💵 Click for Money! 💵";
+button.textContent = "💎 Click for Gems! 💎";
 app.append(button);
+button.addEventListener("click", () => {
+    counter += 1;
+
+    // Update the counter display
+    const counterDisplay = document.getElementById("counter-display");
+    if (counterDisplay) {
+        counterDisplay.textContent = `Gems: ${counter}`;
+    } else {
+        console.error("Counter display not found!");
+    }
+})
